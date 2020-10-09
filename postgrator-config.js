9476b563fa49e774b670e1f69fcdaaa1d798e73f
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2020-10-09 16:10:13 -0500

    prepped deployment urls

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
-  'connectionString': (process.env.NODE_ENV === 'test') ? process.env.TEST_DB_URL : (process.env.NODE_ENV === 'deploy') ? process.env.DEPLOY_DB_URL : process.env.DB_URL,
+  'connectionString': (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : (process.env.NODE_ENV === 'deploy') ? process.env.DEPLOY_DATABASE_URL : process.env.DATABASE_URL,
 };
--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:apple@localhost/dbss',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:apple@localhost/dbss',
   JWT_SECRET: process.env.JWT_SECRET || 'some-secret',
   API_TOKEN: process.env.API_TOKEN || 'some-token',
 };
