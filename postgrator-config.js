f8000afd074ea85c57c67766cb9aacfb861b62d9
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2020-10-09 16:02:37 -0500

    reinstall postgrator outside of dev and set up live db seeding

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
-  'connectionString': (process.env.NODE_ENV === 'test') ? process.env.TEST_DB_URL : process.env.DB_URL,
+  'connectionString': (process.env.NODE_ENV === 'test') ? process.env.TEST_DB_URL : (process.env.NODE_ENV === 'deploy') ? process.env.DEPLOY_DB_URL : process.env.DB_URL,
 };
