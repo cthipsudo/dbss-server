e954e1702e0c9e3a42ad25f1e1b6e4e34004fffd
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2021-11-22 17:13:21 -0600

    removed verbose error for errorhandler, for integrity reasons

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use('/api/scoreboard', scorboardRouter);
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error hello there! :(' }, error };
+    response = { error: { message: 'server error hello there! :(' } };
   } else {
     console.error(error);
     response = { message: error.message, error };
